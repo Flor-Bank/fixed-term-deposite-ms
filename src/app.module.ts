5174492b6a85809c373549131173a8f8274eaa8b
florMignini <flormignini29@gmail.com>
2024-04-04 08:33:21 -0300

    fixed-term microservice config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FixedTermDepositeModule } from './fixed-term-deposite/fixed-term-deposite.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FixedTermDepositeModule],
 })
 export class AppModule {}
