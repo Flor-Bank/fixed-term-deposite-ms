a60edbb467e6815fd2b573b05a496b6882f3ad05
florMignini <flormignini29@gmail.com>
2024-04-27 07:59:16 -0300

    Dto update

--- src/fixed-term-deposite/dto/create-fixed-term-deposite.dto.ts
+++ src/fixed-term-deposite/dto/create-fixed-term-deposite.dto.ts
@@ -1,23 +1,41 @@
 import { fixedTermDepositeStatus } from '@prisma/client';
-import {
-  IsDateString,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
+import { IsDateString, IsEnum, IsNumber, IsPositive } from 'class-validator';
 import { FixedTermDepositeStatusList } from '../enum/fixed-term-deposite.enum';
+import { Type } from 'class-transformer';
 
 export class CreateFixedTermDepositeDto {
-  @IsNumber()
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
   @IsPositive()
-  amount: number;
+  @Type(() => Number)
+  amount_to_invest: number;
   @IsEnum(FixedTermDepositeStatusList, {
     message: `Allowed status values: ${FixedTermDepositeStatusList}`,
   })
-  @IsOptional()
-  status: fixedTermDepositeStatus = fixedTermDepositeStatus.ACTIVE;
-  @IsOptional()
-  @IsDateString() //?or @IsDate()
-  termEndDate: Date;
+  status: fixedTermDepositeStatus;
+  @IsDateString()
+  @Type(() => Date)
+  creation_date: Date;
+  @IsDateString()
+  @Type(() => Date)
+  maturity_date: Date;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  interest_amount: number;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  interest_porcentaje: number;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  total_amount: number;
 }
--- src/fixed-term-deposite/dto/update-fixed-term-deposite.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFixedTermDepositeDto } from './create-fixed-term-deposite.dto';
-
-export class UpdateFixedTermDepositeDto extends PartialType(CreateFixedTermDepositeDto) {
-  id: number;
-}
