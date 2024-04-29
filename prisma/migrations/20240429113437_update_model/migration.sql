329c1f7b31c75421943f1c441156688f84a22539
florMignini <flormignini29@gmail.com>
2024-04-29 08:38:35 -0300

    migration update

--- prisma/migrations/20240429113437_update_model/migration.sql
+++ prisma/migrations/20240429113437_update_model/migration.sql
@@ -6,12 +6,11 @@ CREATE TABLE "FixedTermDeposite" (
     "id" SERIAL NOT NULL,
     "amount_to_invest" DOUBLE PRECISION NOT NULL,
     "status" "fixedTermDepositeStatus" NOT NULL,
-    "InterestRate" DOUBLE PRECISION NOT NULL,
-    "creation_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "maturity_date" TIMESTAMP(3) NOT NULL,
-    "interest_amount" DOUBLE PRECISION NOT NULL,
-    "interest_porcentaje" DOUBLE PRECISION NOT NULL,
+    "interest_rate" DOUBLE PRECISION NOT NULL,
+    "interest_rate_amount" DOUBLE PRECISION NOT NULL,
     "total_amount" DOUBLE PRECISION NOT NULL,
+    "creation_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "FixedTermDeposite_pkey" PRIMARY KEY ("id")
